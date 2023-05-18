a0754b180cff7103c7c8e82b1cc95bfeb10c104e
Piterson25 <piotrmaszczak25@gmail.com>
2023-05-18 10:48:33 +0200

    Lab13

---
+++ Lab13/src/Spotkanie.java
@@ -0,0 +1,40 @@
+import java.time.LocalTime;
+
+public final class Spotkanie extends Zdarzenie {
+
+    private String priority;
+
+    public Spotkanie(String desc, LocalTime startTime, LocalTime endTime, String priority) {
+        this.desc = desc;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return super.desc;
+    }
+
+    public LocalTime getStartTime() {
+        return super.startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return super.endTime;
+    }
+
+    public String getPriority() {
+        return this.priority;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Opis: " + this.desc + "\n");
+        builder.append("(" + this.startTime.toString() + " - " + this.endTime.toString() + ")\n");
+        builder.append("Priorytet: " + this.priority + "\n");
+
+        return builder.toString();
+    }
+
+}
